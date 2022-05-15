d05df9d66d4511884ac97cc1d5b22605119cef19
S0954007 <zy039251762@gmail.com>
2022-05-15 21:21:29 +0800

    v6.4.2

--- login/SignIn/check_buyer.php
+++ login/SignIn/check_buyer.php
@@ -11,7 +11,7 @@
     $password = $_POST['password'];
     $_SESSION['flag'] = "null";
     
-    $link = mysqli_connect('localhost','root','root123456','user') or die("無法開啟MySQL資料庫連結!<br>");
+    $link = mysqli_connect('localhost','root','root123456','group_27') or die("無法開啟MySQL資料庫連結!<br>");
 
     mysqli_query($link, 'SET CHARACTER SET utf8');
     mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
