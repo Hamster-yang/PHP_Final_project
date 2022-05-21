90247898b0865fb526733d0dda8ef73467cdef5e
S0954045 <S0954045@gm.ncue.edu.tw>
2022-05-21 19:36:05 +0800

    v1.2.1

--- buyer/addgoods.php
+++ buyer/addgoods.php
@@ -10,7 +10,7 @@
 		// 送出編碼的MySQL指令
 		mysqli_query($link, 'SET CHARACTER SET utf8');
 		mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
-		$sql = "insert into shopcart values ('" . $member_no . "','" . $goods_No  . "')";	
+		$sql = "insert into shopcart values ('".$member_no."','".$goods_No."')";	
 	
 		echo "<script> {window.alert('加入成功');history.go(-1)} </script>";//返回上頁
 		mysqli_close($link); // 關閉資料庫連結
